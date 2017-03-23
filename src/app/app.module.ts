09bc86eb83d68af35c6e18b76b88f493113aadd1
Cristian Rojas <crarojasca@unal.edu.co>
2017-03-23 00:06:39 -0500

    Landing Components created and organized.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { PromoComponent } from './promo/promo.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    PromoComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
