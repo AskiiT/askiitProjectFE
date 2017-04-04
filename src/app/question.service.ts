8178626a5a5a132fead1b93e5ee85b2a71fcd281
David Guzmán <djguzmanc@unal.edu.co>
2017-04-04 13:01:24 -0500

    Question service added and simple questions view at Wall component.

---
+++ src/app/question.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+@Injectable()
+export class QuestionService {
+
+  constructor( private http: Http ) { }
+
+  log ( ) {
+  	console.log( "I am the question service." );
+  }
+
+  getData( ) {
+  	return this.http.get( 'http://localhost:3000/api/v1/questions' ).map( ( res: Response ) => res.json( ).data );
+  }
+
+}
--- src/app/wall/wall.component.ts
+++ src/app/wall/wall.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../question.service';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-wall',
   templateUrl: './wall.component.html',
-  styleUrls: ['./wall.component.css']
+  styleUrls: ['./wall.component.css'],
+  providers: [ QuestionService ]
 })
 export class WallComponent implements OnInit {
 
-  constructor() { }
+	data: Array<any>;
+	response$;
 
-  ngOnInit() {
-  }
+    constructor( private qService: QuestionService ) { }
+
+    ngOnInit( ) {
+    	this.subscribeData( );
+    }
+
+    test( ) {
+    	this.qService.log( );
+    }
+
+    subscribeData( ) {
+    	this.response$ = this.qService.getData( );
+
+    	this.response$.subscribe( 
+    		res => this.data = res,
+    		() => {},
+    		() => console.log( "OK: completed!" )
+    	);
+    }
 
 }
