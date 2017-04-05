afc83a4939957c810d850095fdb9df1c92bc33d2
David Guzmán <djguzmanc@unal.edu.co>
2017-04-05 11:43:16 -0500

    User service added: Chat component shows all users name.

--- src/app/chats/chats.component.ts
+++ src/app/chats/chats.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-chats',
   templateUrl: './chats.component.html',
-  styleUrls: ['./chats.component.css']
+  styleUrls: ['./chats.component.css'],
+  providers: [ UserService ]
 })
 export class ChatsComponent implements OnInit {
 
-  constructor() { }
+	allUsers: Array<any>;
+	response$;
 
-  ngOnInit() {
-  }
+    constructor( private uService: UserService ) { }
 
+    ngOnInit( ) {
+    	this.subscribeData( );
+    }
+
+    subscribeData( ) {
+    	this.response$ = this.uService.getAllUsers( );
+
+    	this.response$.subscribe( 
+    		res => this.allUsers = res,
+    		() => {},
+    		() => console.log( "OK: completed!" )
+    	);
+    }
+    
 }
