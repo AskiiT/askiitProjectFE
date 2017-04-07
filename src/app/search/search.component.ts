a1474d13eb423f7465947bd6c364cc7bf4080496
David Guzmán <djguzmanc@unal.edu.co>
2017-04-06 20:57:00 -0500

    Topic service added and topics added to search component filter view.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,32 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { TagService } from '../tag.service';
+import { TopicService } from '../topic.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
-  providers: [ TagService ]
+  providers: [ TagService, TopicService ]
 })
 export class SearchComponent implements OnInit {
 
 	allTags: Array<any>;
-	response$;
+  allTopics: Array<any>;
+
+	tagsResponse$;
+  topicsResponse$;
+
   empty:boolean=false;
   term:string;
-    constructor( private tService: TagService ) { }
+    constructor( private tagService: TagService, private topicService: TopicService ) { }
 
     ngOnInit( ) {
     	this.subscribeData( );
     }
 
     subscribeData( ) {
-    	this.response$ = this.tService.getAllTags( );
+    	this.tagsResponse$ = this.tagService.getAllTags( );
 
-    	this.response$.subscribe(
+    	this.tagsResponse$.subscribe(
     		res => this.allTags = res,
     		() => {},
     		() => console.log( "OK: completed!" )
     	);
+
+      this.tagsResponse$ = this.topicService.getAllTopics( );
+
+      this.tagsResponse$.subscribe(
+        res => this.allTopics = res,
+        () => {},
+        () => console.log( "OK: completed!" )
+      );
     }
     isEmpty(){
       if (this.term.length > 0){
---
+++ src/app/topic.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+@Injectable()
+export class TopicService {
+
+  constructor( private http: Http ) { }
+
+  getAllTopics( ) {
+  	return this.http.get( 'http://localhost:3000/api/v1/topics' ).map( ( res: Response ) => res.json( ).data );
+  }
+
+
+}
