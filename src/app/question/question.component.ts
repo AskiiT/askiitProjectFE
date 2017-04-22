c508fc4205c2e6749580296a10bf6a317d3f3c03
David Guzmán <djguzmanc@unal.edu.co>
2017-04-21 22:49:41 -0500

    Some vertical responsiveness added and question slide animation added.

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
+import {style, state, animate, transition, trigger} from '@angular/core';
 
 export interface Question {
     id?: number;
@@ -11,8 +12,22 @@ export interface Question {
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.css'],
+  animations: [
+      trigger('slideIn', [
+        state('*', style({ 'overflow-y': 'hidden' })),
+        state('void', style({ 'overflow-y': 'hidden' })),
+        transition('* => void', [
+            style({ height: '*' }),
+            animate(250, style({ height: 0}))
+        ]),
+        transition('void => *', [
+            style({ height: '0' }),
+            animate(250, style({ height: '*' }))
+        ])
+    ])]
 })
+
 export class QuestionComponent implements OnInit {
 
   @Input() question: Question;
