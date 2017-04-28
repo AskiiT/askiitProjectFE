c2f8a87624a3c7911007d8b8921b3717cab0928e
David Guzmán <djguzmanc@unal.edu.co>
2017-04-28 08:56:36 -0500

    Some style added.

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -28,7 +28,17 @@ export interface Question {
             style({ height: '0' }),
             animate(250, style({ height: '*' }))
         ])
-    ])]
+    ]),
+    trigger('fadeInOut', [
+        transition(':enter', [
+          style({opacity:0}),
+          animate(250, style({opacity:1}))
+        ]),
+        transition(':leave', [
+          animate(250, style({opacity:0}))
+        ])
+    ])
+    ]
 })
 
 export class QuestionComponent implements OnInit {
