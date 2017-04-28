233cbda757394f4aa9976821880f984119e6484c
David Guzmán <djguzmanc@unal.edu.co>
2017-04-27 23:25:36 -0500

    Validations animations added, and user post added.

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 @Injectable()
 export class UserService {
@@ -22,6 +22,26 @@ export class UserService {
   getUsersByMail( subString ) {
       return this.http.get( 'http://localhost:3000/api/v1/users/search/email/' + subString )
         .map( ( res: Response ) => res.json( ).data );
+  }
+
+  postUser( formData, color ) {
+
+      var user = {
+          "email": formData.email,
+          "first_name": formData.first_name,
+          "last_name": formData.last_name,
+          "username": formData.username,
+          "password": formData.password,
+          "color": color
+      }
+
+      console.log( user );
+
+    const headers = new Headers( { 'Content-Type': 'application/json; charset=utf-8' } );
+    const options = new RequestOptions( { headers: headers } );
+
+    return this.http.post( 'http://localhost:3000/api/v1/auth', JSON.stringify( user ), options )
+          .map( ( res: Response ) => res.json( ).data );
   }
 
 }
